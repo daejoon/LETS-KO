fa22dd1c9184c10b560c2a0f80502b73f245d645
Ko DaeJoon <kkode1911@hanmail.net>
2013-12-22 02:20:34 +0900

    modify ExcelFactory.java

--- First/src/main/java/dd2/com/excel/factory/ExcelFactory.java
+++ First/src/main/java/dd2/com/excel/factory/ExcelFactory.java
@@ -1,7 +1,6 @@
 package dd2.com.excel.factory;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.apache.poi.ss.usermodel.Workbook;
@@ -9,24 +8,15 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
-/**
- * @Class Name : ExcelFactory.java
- * @Description : Worksheet 팩토리 클래스
- * @author KDJ
- * @since 2012. 9. 22.
- * @version 1.0
- */
-
 public class ExcelFactory {
     private final static String HSSFWorkbook_EXTNAME = "xls";
     private final static String XSSFWorkbook_EXTNAME = "xlsx";
 
-
-	public static Workbook CreateWorkbook(String fullFilePath) throws FileNotFoundException, IOException {
+	public static Workbook CreateWorkbook(String fullFilePath) throws IOException {
 		Workbook wb = null;
     	if ( fullFilePath != null ) {
     		String[] files = fullFilePath.split("\\.");
-    		if ( files != null && files.length > 0 ) {
+    		if ( files.length > 0 ) {
                 String fileExt = files[files.length-1];
     			
     			if ( fileExt.toLowerCase().equals(HSSFWorkbook_EXTNAME) ) {
