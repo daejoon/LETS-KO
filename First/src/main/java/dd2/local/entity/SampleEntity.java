9bcbc9fb36fc9984df5551b9f8e72eb4a64d86d5
daejoon <kkode1911@gmail.com>
2014-01-19 20:56:05 +0900

    Modifed README.md

--- First/src/main/java/dd2/local/entity/SampleEntity.java
+++ First/src/main/java/dd2/local/entity/SampleEntity.java
@@ -3,6 +3,7 @@ package dd2.local.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 /**
  * Created by kdj on 2014. 1. 18..
@@ -47,6 +48,11 @@ public class SampleEntity {
         this.age = age;
     }
 
+    /**
+     * @JsonIgnore를 사용하면 특정 필드를 Deserialize에서 제외할수 있다.
+     * @return
+     */
+//    @JsonIgnore
     @Column(name = "DESCRIPTION", nullable = true)
     public String getDescription() {
         return description;
