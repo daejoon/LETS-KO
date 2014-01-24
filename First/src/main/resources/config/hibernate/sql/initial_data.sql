34dee22e466236534e1cb4ca5baa49489ed9ffbc
daejoon <kkode1911@gmail.com>
2014-01-24 18:09:53 +0900

    git commit

--- First/src/main/resources/config/hibernate/sql/initial_data.sql
+++ First/src/main/resources/config/hibernate/sql/initial_data.sql
@@ -1,5 +1,5 @@
 INSERT INTO TB_CODE_ROLE(NAME, REMARK) VALUES('ROLE_ADMIN'      , 'ADMINISTRATOR USER');
 INSERT INTO TB_CODE_ROLE(NAME, REMARK) VALUES('ROLE_USER'       , 'GENERAL USER');
-INSERT INTO TB_CODE_ROLE(NAME, REMARK) VALUES('ROLE_ANONYMOUS' , 'ANONYMOUS USER');
+INSERT INTO TB_CODE_ROLE(NAME, REMARK) VALUES('ROLE_ANONYMOUS'  , 'ANONYMOUS USER');
 INSERT INTO AD_Company (AD_Company_ID, Name, Value, Description, isActive) VALUES ( 1, 'admin_company', 'admin_company', 'default company', 'Y');
 INSERT INTO AD_User (AD_User_ID, AD_Company_ID, Name, Password, Description, isActive) VALUES (1, 1, 'admin', 'admin', 'default user', 'Y');
