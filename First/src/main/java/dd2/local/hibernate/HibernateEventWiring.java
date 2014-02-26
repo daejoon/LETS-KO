dd24c9c017b0a758ddd38538e3adb7fc9dd3a749
daejoon <kkode1911@gmail.com>
2014-02-26 22:14:19 +0900

    Hibernate4 Event Class와 Config 추가

--- First/src/main/java/dd2/local/hibernate/HibernateEventWiring.java
+++ First/src/main/java/dd2/local/hibernate/HibernateEventWiring.java
@@ -1,13 +1,12 @@
 package dd2.local.hibernate;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.hibernate.SessionFactory;
 import org.hibernate.event.service.spi.EventListenerRegistry;
 import org.hibernate.event.spi.EventType;
 import org.hibernate.internal.SessionFactoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
@@ -15,6 +14,7 @@ import javax.annotation.PostConstruct;
  * Created by kdj on 2014. 2. 26..
  */
 public class HibernateEventWiring {
+    private static final Log logger = LogFactory.getLog(HibernateEventWiring.class);
 
     @Autowired
     private SessionFactory sessionFactory;
