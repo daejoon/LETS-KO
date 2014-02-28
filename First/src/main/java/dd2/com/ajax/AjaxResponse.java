ea0ff694c87c94a60b2e136f76a549b83cd69928
daejoon <kkode1911@gmail.com>
2014-03-01 01:14:37 +0900

    JavaDoc Test

--- First/src/main/java/dd2/com/ajax/AjaxResponse.java
+++ First/src/main/java/dd2/com/ajax/AjaxResponse.java
@@ -3,42 +3,58 @@ package dd2.com.ajax;
 import java.io.Serializable;
 
 /**
- * Created by KDJ on 13. 12. 9.
+ * 일괄적인 Json 구조를 위한 Generic Class
+ * @param <T> Json으로 생성하고자 하는 POJO 및 Collection의 Data Model
  */
 public class AjaxResponse<T> implements Serializable {
     private boolean status = false;
     private String message = "";
     private T data;
 
+    /**
+     *
+     * @return ture: 성공, false: 실패
+     */
     public boolean isStatus() {
         return status;
     }
 
     /**
-     * true : 성공
-     * false : 실패
-     * @param status
+     *
+     * @param status 실패: true, 성공: false
      */
     public void setStatus(boolean status) {
         this.status = status;
     }
 
+    /**
+     *
+     * @return 메세지를 리턴한다.
+     */
     public String getMessage() {
         return message;
     }
 
     /**
-     * 실패시 메세지
-     * @param message
+     *
+     * @param message 메세지를 설정한다.
      */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /**
+     *
+     * @return Data Model을 리턴한다.
+     */
     public T getData() {
         return data;
     }
 
+    /**
+     *
+     * @param data Data Model을 설정한다.
+     */
     public void setData(T data) {
         this.data = data;
     }
